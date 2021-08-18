3432a0ae0313a44cf10ace77a044a38cc951a6ff
PrakashTech <prakashd88@gmail.com>
2021-08-18 19:11:59 +0530

    netlify configuration added

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,24 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		
+		adapter: adapter()
 	}
 };
+
+// import adapter from '@sveltejs/adapter-node';
+
+// export default {
+// 	kit: {
+
+// 		adapter: adapter({
+// 			// default options are shown
+// 			out: 'buildTest',
+// 			precompress: false
+// 		})
+// 	}
+// };
 
 export default config;
